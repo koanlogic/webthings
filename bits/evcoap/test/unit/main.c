a29dc5ce555bf2a9d6e7ab6aa63f586312ee32fd
tho <tho@koanlogic.com>
2011-12-20 11:23:18 +0100

    [evcoap] added unit test skeleton under test/unit

---
+++ bits/evcoap/test/unit/main.c
@@ -0,0 +1,24 @@
+#include <u/libu.h>
+
+int facility = LOG_LOCAL0;
+
+int test_suite_options_register(u_test_t *t);
+
+int main(int argc, char **argv)
+{
+    int rc;
+    u_test_t *t = NULL;
+
+    con_err_if (u_test_new("evcoap unit tests", &t));
+
+    con_err_if (test_suite_options_register(t));
+
+    rc = u_test_run(argc, argv, t);
+    u_test_free(t);
+
+    return rc;
+err:
+    u_test_free(t);
+    return EXIT_FAILURE;
+}
+
---
+++ bits/evcoap/test/unit/options.c
@@ -0,0 +1,27 @@
+#include <u/libu.h>
+#include "evcoap_prv.h"
+
+//int test_suite_options_register(u_test_t *t);
+
+static int test_todo(u_test_case_t *tc);
+
+static int test_todo(u_test_case_t *tc)
+{
+    return U_TEST_SUCCESS;
+}
+
+int test_suite_options_register(u_test_t *t)
+{
+    u_test_suite_t *ts = NULL;
+
+    con_err_if (u_test_suite_new("CoAP options", &ts));
+
+    con_err_if (u_test_case_register("TODO", test_todo, ts));
+
+    /* No dependencies. */
+
+    return u_test_suite_add(ts, t);
+err:
+    u_test_suite_free(ts);
+    return -1;
+}
