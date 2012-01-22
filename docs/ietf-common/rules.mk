80a6525b49c17a80fbccee39e197765b6bb36ae3
stewy <stewy@koanlogic.com>
2012-01-22 23:05:07 +0100

    docs/ietf-common: check for missing prerequisites

--- docs/ietf-common/rules.mk
+++ docs/ietf-common/rules.mk
@@ -1,5 +1,13 @@
 TXT = $(XML:.xml=.txt)
 
+# check prerequisites
+ifndef TCLSH
+	$(error missing prerequisite: 'tclsh'!)
+endif
+ifndef XML2RFC
+	$(error missing prerequisite: 'xm2rfc'!)
+endif
+
 all: $(TXT)
 
 %.txt: %.xml $(XML2RFC) ; $(TCLSH) $(XML2RFC) xml2rfc $< $@
