4b481f0021de7840210cd1e8bb473bb0df277b17
tho <tho@koanlogic.com>
2013-10-08 15:56:55 +0100

    geo I-D -00

--- docs/ietf-common/rules.mk
+++ docs/ietf-common/rules.mk
@@ -1,10 +1,6 @@
 TXT = $(XML:.xml=.txt)
 HTML = $(XML:.xml=.html)
 
-# check prerequisites
-ifndef TCLSH
-	$(error missing prerequisite: 'tclsh'!)
-endif
 ifndef XML2RFC
 	$(error missing prerequisite: 'xm2rfc'!)
 endif
@@ -14,8 +10,8 @@ endif
 
 all: $(TXT) post
 
-post: ; cp $(TXT) ~/Sites/ && $(RFCMARKUP) url=http://localhost/~$(shell whoami)/$(TXT) > ~/Sites/$(HTML)
+post: ; cp $(TXT) ~/web/I-Ds/ && $(RFCMARKUP) url=http://localhost/~$(shell whoami)/I-Ds/$(TXT) > ~/web/I-Ds/$(HTML)
 
-%.txt: %.xml $(XML2RFC) ; $(TCLSH) $(XML2RFC) xml2rfc $< $@
+%.txt: %.xml $(XML2RFC) ; $(XML2RFC) $< $@
 
 clean: ; $(RM) $(TXT)
--- docs/ietf-common/tools.mk
+++ docs/ietf-common/tools.mk
@@ -1,5 +1,5 @@
 TCLSH = /usr/bin/tclsh
 
-XML2RFC = $(HOME)/bin/xml2rfc.tcl
+XML2RFC = $(HOME)/bin/xml2rfc
 
 RFCMARKUP = $(HOME)/bin/rfcmarkup
